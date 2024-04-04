23dc320ebd08341b8185bff392aa65518074c528
ankit-gujar1 <136838138+ankit-gujar1@users.noreply.github.com>
2024-04-04 15:38:14 +0530

    Update useAuthContext.js

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,4 +1,4 @@
-//useAuthContext just call AuthContext and store the stuff in useAuthContext, when we use use AuthContext we get things which are in AuthContext like user data
+//useAuthContext just call AuthContext and store the stuff in useAuthContext, when we use useAuthContext we get things which are in AuthContext like user data
 
 import { AuthContext } from "../context/AuthContext"
 import { useContext } from "react"
