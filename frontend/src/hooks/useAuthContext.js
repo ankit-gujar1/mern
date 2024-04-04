e8d61e851cb23ef0c54875d2038ad69287c8eb43
ankit-gujar1 <136838138+ankit-gujar1@users.noreply.github.com>
2024-04-04 15:43:48 +0530

    Update useAuthContext.js

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -4,7 +4,7 @@ import { AuthContext } from "../context/AuthContext"
 import { useContext } from "react"
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext); //get data from AuthContext and return that data
 
   if(!context) {
     throw Error('useAuthContext must be used inside an AuthContextProvider')
